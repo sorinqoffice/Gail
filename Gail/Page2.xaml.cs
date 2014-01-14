b8cf5ae16c1213ede66e84f309d3d8acf1338d9d
sorinqoffice <sorin@qoffice.ro>
2014-01-14 02:29:01 +0200

    Commit List Picker 2

--- Gail/Page2.xaml.cs
+++ Gail/Page2.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using System.Collections.ObjectModel;
+
 namespace Gail
 {
     public partial class Page2 : PhoneApplicationPage
@@ -18,14 +19,32 @@ namespace Gail
         public Page2()
         {
             InitializeComponent();
+            ListNumber = new ObservableCollection<ItemModel>();
             this.Loaded +=new RoutedEventHandler(Page2_Loaded);
         }
 
         private void Page2_Loaded(object sender, RoutedEventArgs e)
         {
+            if (ListNumber.Count == 0)
+            {
+                string[] races = new string[9] {"White", "Black","Hispanic", "Chinese", "Japanese", "Philipino", "Hawaiian", "Oceanic", "Other/Asian - American"}
 
+                foreach (string raceType in races)
+                {
+                    ListNumber.Add(new ItemModel {Race = raceType});
+                }
+            }
         }
 
+        public static ObservableCollection<ItemModel> ListNumber {get; private set;}
+
+        private void ContentPanel_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+        
+        }
+        {
+
+        }
 
         private void ApplicationBarIconButton_Click(object sender, EventArgs e)
         {
